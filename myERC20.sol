f0dab5e4f560c59c95ce6088de139cec4f66372d
rajpatil1300 <rajpatil8931@gmail.com>
2025-02-23 21:24:51 +0530

    Update myERC20.sol

--- myERC20.sol
+++ myERC20.sol
@@ -1,5 +1,4 @@
-//SPDX-License-Identifier: MIT
-
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.26;
 
 contract myERC20 {
@@ -7,13 +6,13 @@ contract myERC20 {
     string public symbol;
     uint8 public immutable decimals;
     uint256 public immutable totalSupply;
-    mapping(address=>uint256) _balances;
-    mapping(address=>mapping(address=>uint256)) _allowances;
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
 
-    event Transfer(address indexed _from,address indexed _to,uint256 _value);
-    event Approval(address indexed _owner,address indexed _spender,uint256 _value);
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     
-    constructor(string memory _name,string memory _symbol,uint _totalSupply){
+    constructor(string memory _name, string memory _symbol, uint _totalSupply) {
         name = _name;
         symbol = _symbol;
         decimals = 18;
@@ -21,39 +20,39 @@ contract myERC20 {
         _balances[msg.sender] = _totalSupply;
     }
 
-    function balanceOf(address _owner) public view returns(uint256){
+    function balanceOf(address _owner) public view returns (uint256) {
         return _balances[_owner];  
     }
 
-    function transfer(address _to,uint256 _value)public returns(bool){
-        require((_balances[msg.sender] >= _value) && (_balances[msg.sender] >0));
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        require(_balances[msg.sender] >= _value, "Insufficient balance");
         _balances[msg.sender] -= _value;
         _balances[_to] += _value;
-        emit Transfer(msg.sender,_to,_value);
+        emit Transfer(msg.sender, _to, _value);
         return true;
     }
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
-    require(_allowances[_from][msg.sender] >= _value, "Not allowed");
-    require(_balances[_from] >= _value, "Insufficient balance");
+        require(_allowances[_from][msg.sender] >= _value, "Not allowed");
+        require(_balances[_from] >= _value, "Insufficient balance");
 
-    _allowances[_from][msg.sender] -= _value;
-    _balances[_from] -= _value;
-    _balances[_to] += _value;
+        _allowances[_from][msg.sender] -= _value;
+        _balances[_from] -= _value;
+        _balances[_to] += _value;
 
-    emit Transfer(_from, _to, _value);
-    return true;
-}
-
-    
-    function approve(address _spender,uint256 _value) public returns(bool){
-       require(_balances[msg.sender] >= _value);
-       _allowances[_spender][msg.sender] = _value;
-       emit Approval(msg.sender, _spender, _value); 
-       return true;
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 
-    function allowance(address _owner,address _spender) public view returns(uint256){
-        return _allowances[_spender][_owner];
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        require(_balances[msg.sender] >= _value, "Insufficient balance");
+        _allowances[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value); 
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256) {
+        return _allowances[_owner][_spender];
     }
 }
+
